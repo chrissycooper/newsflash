1c347d182c5265711d0b4e7d0eb4c5ac33c3a15f
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-06-06 16:17:43 -0500

    Clean up unnecessary template code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import topHeadlinesSample from '../mockdata';
 import './App.css';
 
 function App() {
+  const [articles, setArticles] = useState({})
+
+  useEffect(() => {
+    setArticles(topHeadlinesSample)
+  }, [])
+
+  useEffect(()=> {
+    console.log('second useeffect', articles)
+  }, [articles])
+
   return (
     <div className="App">
-      
+      <h1>Welcome to NewsFlash ⚡︎</h1>
     </div>
   );
 }
