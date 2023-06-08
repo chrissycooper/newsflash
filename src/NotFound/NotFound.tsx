e90eaa23a9451828437b946940a747d38cae6439
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-06-08 15:54:24 -0500

    Reorganize files

---
+++ src/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './NotFound.css';
+
+const NotFound = () => {
+  return (
+    <div className="err-message">
+      <p>Uh Oh, something unexpected happened</p>
+    </div>
+  )
+}
+
+export default NotFound;
